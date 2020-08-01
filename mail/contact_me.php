319730700dd4babbb7caab7f310c7b861ce4d303
Prodip Ghosh <33126657+engg-prodip@users.noreply.github.com>
2020-08-01 21:53:38 +0600

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,6 @@
 <?php
 // Check for empty fields
+$status = false;
 if(empty($_REQUEST['name'])      ||
    empty($_REQUEST['email'])     ||
    empty($_REQUEST['phone'])     ||
@@ -7,23 +8,25 @@ if(empty($_REQUEST['name'])      ||
    !filter_var($_REQUEST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
-   return false;
+   return $status;
    }else{
         $name = strip_tags(htmlspecialchars($_REQUEST['name']));
         $email_address = strip_tags(htmlspecialchars($_REQUEST['email']));
         $phone = strip_tags(htmlspecialchars($_REQUEST['phone']));
         $message = strip_tags(htmlspecialchars($_REQUEST['message']));
-        $message = wordwrap($message,70);   
+        $message = wordwrap($message,70);
+        include("http://shadinmart.com/prodip/mail.php?name=$name&email=$email_address&phone=$phone&message=$message");
+         $status = true;
         // Create the email and send the message
-        $to = 'engg.prodip@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-        $email_subject = "Github Contact Form:  $name, Email: $email_address, Phone: $phone";
-        $email_body = "$message";
+        //$to = 'engg.prodip@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+        //$email_subject = "Github Contact Form:  $name, Email: $email_address, Phone: $phone";
+        //$email_body = "$message";
         // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
         // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "Reply-To: $email_address";   
-        mail($to,$email_subject,$email_body, $headers);
-        return true;  
+        //$headers = "MIME-Version: 1.0" . "\r\n";
+        //$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        //$headers .= "Reply-To: $email_address";   
+        //mail($to,$email_subject,$email_body, $headers);
+        return $status;
    }
 ?>
