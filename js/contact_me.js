a3beea7a7d332c405d4dd1b6edce4273c2b0d2a3
Prodip Ghosh <33126657+engg-prodip@users.noreply.github.com>
2020-08-01 22:26:35 +0600

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,8 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        //url: "././mail/contact_me.php",
+        url: "http://shadinmart.com/prodip/mail.php?name=$name&email=$email&phone=$phone&message=$message",
         type: "POST",
         data: {
           name: name,
