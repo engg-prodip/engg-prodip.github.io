08fb15213e3a14316d36708fd61e2125d9227180
Prodip Ghosh <33126657+engg-prodip@users.noreply.github.com>
2020-08-01 17:45:17 +0600

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "http://shadinmart.com/prodip/mail.php",
         type: "POST",
         data: {
           name: name,
@@ -43,11 +43,22 @@ $(function() {
         },
         error: function() {
           // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+         // $('#success').html("<div class='alert alert-danger'>");
+         // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+         //   .append("</button>");
+         // $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          //$('#success > .alert-danger').append('</div>');
+          //clear all fields
+         // $('#contactForm').trigger("reset");
+
+    // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
