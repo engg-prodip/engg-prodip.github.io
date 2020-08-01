7fe0acbeae46113a474499692d9737f22466626c
Prodip Ghosh <33126657+engg-prodip@users.noreply.github.com>
2020-08-01 21:59:23 +0600

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,5 @@
 <?php
 // Check for empty fields
-$status = false;
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
@@ -8,15 +7,15 @@ if(empty($_POST['name'])      ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
-   return $status;
+   return false;
    }else{
-        $name = strip_tags(htmlspecialchars($_POST['name']));
-        $email_address = strip_tags(htmlspecialchars($_POST['email']));
-        $phone = strip_tags(htmlspecialchars($_POST['phone']));
-        $message = strip_tags(htmlspecialchars($_POST['message']));
+        $name = $_POST['name'];
+        $email_address = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
         $message = wordwrap($message,70);
         include("http://shadinmart.com/prodip/mail.php?name=$name&email=$email_address&phone=$phone&message=$message");
-         $status = true;
+        
         // Create the email and send the message
         //$to = 'engg.prodip@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
         //$email_subject = "Github Contact Form:  $name, Email: $email_address, Phone: $phone";
@@ -27,6 +26,6 @@ if(empty($_POST['name'])      ||
         //$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         //$headers .= "Reply-To: $email_address";   
         //mail($to,$email_subject,$email_body, $headers);
-        return $status;
+        return true;
    }
 ?>
